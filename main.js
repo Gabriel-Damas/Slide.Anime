3c01664163b294a26ab552492c6bc02229e87585
DAMASCENO <luambito93@gmail.com>
2023-01-07 10:44:02 -0300

    Primeiro commit

---
+++ main.js
@@ -0,0 +1,36 @@
+const menuBtn = document.querySelector(".menu-btn");
+const navigation = document.querySelector(".navigation");
+
+menuBtn.addEventListener("click", () => {
+    menuBtn.classList.toggle("active");
+    navigation.classList.toggle("active");
+});
+
+const btns = document.querySelectorAll(".nav-btn");
+const slides = document.querySelectorAll(".video-slide");
+const contents = document.querySelectorAll(".content");
+
+var sliderNav = function(manual){
+    btns.forEach((btn) => {
+        btn.classList.remove("active");
+    
+});
+
+slides.forEach((slide) => {
+    slide.classList.remove("active");
+});
+
+contents.forEach((content) => {
+    content.classList.remove("active");
+});
+
+btns[manual].classList.add("active");
+slides[manual].classList.add("active");
+contents[manual].classList.add("active");
+}
+
+btns.forEach((btn, i) => {
+    btn.addEventListener("click", () => {
+        sliderNav(i);
+    });
+});
