656574c4ff93736bd371fd076398f61721950c8e
DAMASCENO <luambito93@gmail.com>
2023-01-08 08:08:45 -0300

    Alteração no Js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ menuBtn.addEventListener("click", () => {
     menuBtn.classList.toggle("active");
     navigation.classList.toggle("active");
 });
-
+ 
 const btns = document.querySelectorAll(".nav-btn");
 const slides = document.querySelectorAll(".video-slide");
 const contents = document.querySelectorAll(".content");
