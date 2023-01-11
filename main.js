29a1fe634d9c1d5a9080c5b18f66309e461375b6
DAMASCENO <luambito93@gmail.com>
2023-01-11 08:01:34 -0300

    Alteração no Js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const menuBtn = document.querySelector(".menu-btn");
 const navigation = document.querySelector(".navigation");
-
+ 
 menuBtn.addEventListener("click", () => {
     menuBtn.classList.toggle("active");
     navigation.classList.toggle("active");
